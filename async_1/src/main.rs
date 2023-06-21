92fc679e720bbfa04e58fbe8dca2a6082f6c47db
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-06-21 09:41:32 +0200

    async parse from_list

--- async_1/src/main.rs
+++ async_1/src/main.rs
@@ -1,48 +1,28 @@
-// use reqwest;
-
-
-// fn main() {
-//     let urls = [
-//         "https://www.google.com/",
-//         "https://www.github.com/",
-//         "https://www.wikipedia.org/",
-//         "https://www.youtube.com/",
-//         "https://www.stackoverflow.com/"
-//     ];
-
-//     for i in 0..urls.len() {
-//         let url = urls[i];
-//         let mut req = reqwest::blocking::get(url).unwrap();
-//         println!("{:?}", url);
-//     }
-// }
-
-
+use reqwest::Url;
 use std::error::Error;
 use tokio::task;
-use reqwest::Url;
 
 #[tokio::main]
 async fn main() {
     println!("-=-=-=--=- Отдельные варианты -=-=-=-=-");
     single_task().await.unwrap();
     few_tasks().await;
+    from_list().await;
 }
 
 async fn single_task() -> Result<(), Box<dyn Error>> {
-    println!("single_task Before sleep");
+    println!("single_task Before");
     let url = Url::parse("https://www.google.com").unwrap();
-    let _response = reqwest::get(url).await?;    
-    println!("single_task After sleep");
+    let _response = reqwest::get(url).await?;
+    println!("single_task After");
     Ok(())
 }
-
 
 async fn few_tasks() {
     let task1 = async {
         println!("few_tasks 1 START");
         let url = Url::parse("https://www.github.com/").unwrap();
-        let response = reqwest::get(url).await.unwrap();    
+        let response = reqwest::get(url).await.unwrap();
         println!("few_tasks 1 FIN");
         response
     };
@@ -50,7 +30,7 @@ async fn few_tasks() {
     let task2 = async {
         println!("few_tasks 2 START");
         let url = Url::parse("https://www.wikipedia.org/").unwrap();
-        let response = reqwest::get(url).await.unwrap();    
+        let response = reqwest::get(url).await.unwrap();
         println!("few_tasks 2 FIN");
         response
     };
@@ -58,4 +38,30 @@ async fn few_tasks() {
     let (res1, res2) = tokio::join!(task1, task2);
 
     println!("{:?},  {:?}", res1.status(), res2.status());
+}
+
+async fn from_list() {
+    let urls = [
+        "https://www.google.com/",
+        "https://www.github.com/",
+        "https://www.wikipedia.org/",
+        "https://www.youtube.com/",
+        "https://www.stackoverflow.com/"
+    ];
+
+    let mut tasks = Vec::new();
+    for line in urls {
+        let t: task::JoinHandle<reqwest::Response> = task::spawn(async move {
+            println!("from_list {} START", line);
+            let url = Url::parse(line).unwrap();
+            let response = reqwest::get(url).await.unwrap();
+            println!("from_list {} FIN {}", line, response.status());
+            response
+        });
+        tasks.push(t);
+    }
+    for task in tasks {
+        task.await.unwrap();
+    }
+
 }
