12b459eab587a6d20634b963a026351c6e55d946
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-06-21 08:22:13 +0200

    async parsing single_task

--- async_1/src/main.rs
+++ async_1/src/main.rs
@@ -1,18 +1,37 @@
-use reqwest;
+// use reqwest;
 
 
-fn main() {
-    let urls = [
-        "https://www.google.com/",
-        "https://www.github.com/",
-        "https://www.wikipedia.org/",
-        "https://www.youtube.com/",
-        "https://www.stackoverflow.com/"
-    ];
+// fn main() {
+//     let urls = [
+//         "https://www.google.com/",
+//         "https://www.github.com/",
+//         "https://www.wikipedia.org/",
+//         "https://www.youtube.com/",
+//         "https://www.stackoverflow.com/"
+//     ];
 
-    for i in 0..urls.len() {
-        let url = urls[i];
-        let mut req = reqwest::blocking::get(url).unwrap();
-        println!("{:?}", url);
-    }
+//     for i in 0..urls.len() {
+//         let url = urls[i];
+//         let mut req = reqwest::blocking::get(url).unwrap();
+//         println!("{:?}", url);
+//     }
+// }
+
+
+use std::error::Error;
+use tokio::task;
+use reqwest::Url;
+
+#[tokio::main]
+async fn main() {
+    println!("-=-=-=--=- Отдельные варианты -=-=-=-=-");
+    single_task().await.unwrap();
+}
+
+async fn single_task() -> Result<(), Box<dyn Error>> {
+    println!("single_task Before sleep");
+    let url = Url::parse("https://www.google.com").unwrap();
+    let _response = reqwest::get(url).await?;    
+    println!("single_task After sleep");
+    Ok(())
 }
