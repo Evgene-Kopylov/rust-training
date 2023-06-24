449ca8b6a3ab860d253e4c02190795b210a0caa9
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-06-24 20:28:19 +0200

    threads_with_shared_mutex

--- thread_2/src/main.rs
+++ thread_2/src/main.rs
@@ -1,3 +1,29 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    threads_with_shared_mutex();
+}
+
+
+fn threads_with_shared_mutex() {
+    let target = vec![];
+    let counter = Arc::new(Mutex::new(target));
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut val = counter.lock().unwrap();
+
+            val.push(i);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {:?}", *counter.lock().unwrap());
 }
