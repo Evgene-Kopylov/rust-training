04c94d90e901d22e3a2e8ee6f0dffbf73998821b
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-06-24 20:44:31 +0200

    многопоточность с каналом

--- thread_2/src/main.rs
+++ thread_2/src/main.rs
@@ -1,11 +1,28 @@
+use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    threads_with_shared_mutex();
+    // threads_with_shared_mutex();
+    threads_with_channal()
 }
 
-fn threads_with_shared_mutex() {
+fn threads_with_channal() {
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..10 {
+        let tx_i = tx.clone();
+        thread::spawn(move || {
+            tx_i.send(i).unwrap();
+        });
+    }
+
+    for received in rx {
+        println!("{}", received);
+    }
+}
+
+fn _threads_with_shared_mutex() {
     let target = vec![];
     let counter = Arc::new(Mutex::new(target));
     let mut handles = vec![];
@@ -26,4 +43,3 @@ fn threads_with_shared_mutex() {
 
     println!("Result: {:?}", *counter.lock().unwrap());
 }
-
